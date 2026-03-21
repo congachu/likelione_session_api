7aa122b513d2494b71b1904ff56f348c3fd17898
congachu <songwlals050328@gmail.com>
2026-03-22 01:02:09 +0900

    feat: BaseEntity

---
+++ src/test/java/likelion/kmu/session/SessionApplicationTests.java
@@ -0,0 +1,13 @@
+package likelion.kmu.session;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SessionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
