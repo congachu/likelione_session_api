ca2ef536e59466f67d31b207f109a2f462f84b6d
congachu <songwlals050328@gmail.com>
2026-03-22 01:20:43 +0900

    feat: check api

---
+++ src/main/java/likelion/kmu/session/check/controller/CheckController.java
@@ -0,0 +1,21 @@
+package likelion.kmu.session.check.controller;
+
+import likelion.kmu.session.check.entity.dto.response.CheckResponseDTO;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
+
+@RestController
+@RequestMapping("/api/check")
+public class CheckController {
+
+    @GetMapping("/")
+    @ResponseBody
+    public ResponseEntity<CheckResponseDTO> check(){
+        return ResponseEntity.ok(new  CheckResponseDTO("ok"));
+    }
+}
