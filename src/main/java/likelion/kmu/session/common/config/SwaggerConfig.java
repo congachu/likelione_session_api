85ef1d4cb094b5ba088d52d135da5bb3a465924f
congachu <songwlals050328@gmail.com>
2026-05-11 19:28:37 +0900

    feat: swagger mixed content

--- src/main/java/likelion/kmu/session/common/config/SwaggerConfig.java
+++ src/main/java/likelion/kmu/session/common/config/SwaggerConfig.java
@@ -1,10 +1,17 @@
 package likelion.kmu.session.common.config;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+@OpenAPIDefinition(
+        servers = {
+                @Server(url = "https://congachu.dev")
+        }
+)
 @Configuration
 public class SwaggerConfig {
 
